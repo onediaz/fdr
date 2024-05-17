51ff044109f2bfff2433db8f14b778e5898ef506
onediaz <juand4535@gmail.com>
2024-05-17 10:11:23 -0500

    URL for deployment

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
       }
 
       // If the token exists, verify it with the auth server to see if it is valid
-      fetch('http://localhost:3080/verify', {
+      fetch(`${REACT_APP_API_URL}/verify`, {
         method: 'POST',
         headers: {
           'jwt-token': user.token,
