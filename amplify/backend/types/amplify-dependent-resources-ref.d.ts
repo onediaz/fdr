980a630a07067b8f69830e370ce4a531fc1a433a
onediaz <juand4535@gmail.com>
2024-05-20 13:14:06 -0500

    auth changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,8 +10,10 @@ export type AmplifyDependentResourcesAttributes = {
     "fdramplified4facb73e": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
