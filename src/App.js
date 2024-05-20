186a100dc2606473c48fc63d9c2ab2c7c98b277f
onediaz <juand4535@gmail.com>
2024-05-20 13:17:06 -0500

    redo of app.js

--- src/App.js
+++ src/App.js
@@ -38,6 +38,33 @@ export const REACT_APP_API_URL = 'https://main.d6kv4iz3qclfx.amplifyapp.com';
 // export const REACT_APP_API_URL = 'http://localhost:3080';
 
 function App({ signOut, user }) {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
+  const [isAdmin, setAdmin] = useState(false)
+  
+  useEffect(() => {
+      // Fetch the user email and token from local storage
+      const user = JSON.parse(localStorage.getItem('user'))
+
+      // If the token/email does not exist, mark the user as logged out
+      if (!user || !user.token) {
+        setLoggedIn(false)
+        return
+      }
+
+      // If the token exists, verify it with the auth server to see if it is valid
+      fetch(`${REACT_APP_API_URL}/verify`, {
+        method: 'POST',
+        headers: {
+          'jwt-token': user.token,
+        },
+      })
+        .then((r) => r.json())
+        .then((r) => {
+          setLoggedIn('success' === r.message)
+          setEmail(user.email || '')
+        })
+    }, [])
   return (
     <div className="App">
       <Router>
