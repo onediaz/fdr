2d63a3e211e87e35ccf3efd0f3e3f517dccd20da
onediaz <juand4535@gmail.com>
2024-05-23 20:57:57 -0500

    dashboard viewing

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -4,43 +4,76 @@ import './dashboard.css';
 import React from "react";
 import { useState, useEffect } from 'react'; 
 import { useParams } from "react-router-dom";
-import { REACT_APP_API_URL } from '../App';
 import { fetchUserAttributes } from '@aws-amplify/auth'; // Import for user data access
-import { getStudent, listStudents } from '../graphql/queries';
+import { listStudents } from '../graphql/queries';
 import { generateClient } from "aws-amplify/api";
 const client = generateClient();
 
 const Dashboard = (props) => {
-    const propEmail = props.email;
-    const { dashboardEmail} = useParams();
+    // const propEmail = props.email;
+    const dashboardEmail = useParams();
     const [balance, setBalance] = useState('');
-    const [studentBalance, setStudentBalance] = useState('');
-    const [propStudentBalance, setpropStudentBalance] = useState('');
+    // const [studentBalance, setStudentBalance] = useState('');
+    // const [propStudentBalance, setpropStudentBalance] = useState('');
     const [balanceError, setBalanceError] = useState('');
-    const [student, setStudent] = useState([]);
-    const [propStudent, setPropStudent] = useState([]);
     const [currentStudent, setCurrentStudent] = useState(null);
+    const [currentStudentBalance, setCurrentStudentBalance] = useState(null);
+    const [currentStudentEmail, setCurrentStudentEmail] = useState(null);
     const [dashboardStudent, setDashboardStudent] = useState(null);
+    const [dashboardStudentBalance, setDashboardStudentBalance] = useState(null);
+    const [dashboardStudentEmail, setDashboardStudentEmail] = useState(null);
 
     useEffect(() => {
         const fetchUser = async () => {
         try {
-            // const currentUser = await Auth.currentAuthenticatedUser();
-            const currentUser = findStudent((await fetchUserAttributes()).email);
-            const dashboardUser = findStudent(dashboardEmail);
-
-            if (currentUser.data.listStudents.items.length > 0) {
-                setCurrentStudent(currentUser.data.listStudents.items[0]);
-                console.log(currentStudent); // Access the first student
-            } else {
-                console.log("No student found with that email.");
+            const tempUser = await fetchUserAttributes();
+            // console.log(tempUser);
+            if(tempUser){
+                const currentUser = await client.graphql({
+                    query: listStudents,
+                    variables: {
+                        filter: {
+                            email: {
+                                eq: tempUser.email
+                            }
+                        }
+                    }
+                  });
+                if (currentUser.data.listStudents.items.length > 0) {
+                    setCurrentStudent(currentUser.data.listStudents.items[0]);
+                    setCurrentStudentBalance(currentStudent.balance);
+                    setCurrentStudentEmail(currentStudent.email);
+                } else {
+                    console.log("No student found with that email.");
+                }
             }
-            if (dashboardUser.data.listStudents.items.length > 0) {
-                setDashboardStudent(dashboardUser.data.listStudents.items[0]);
-                console.log(dashboardStudent); // Access the first student
-            } else {
-                console.log("No student found with that email.");
+            // console.log(dashboardEmail)
+            if(dashboardEmail.email !== ""){
+                const dashhboardUser = await client.graphql({
+                    query: listStudents,
+                    variables: {
+                        filter: {
+                            email: {
+                                eq: dashboardEmail.email
+                            }
+                        }
+                    }
+                  });
+                  if (dashhboardUser.data.listStudents.items.length > 0) {
+                    setDashboardStudent(dashhboardUser.data.listStudents.items[0]);
+                    setDashboardStudentBalance(dashboardStudent.balance);
+                    setDashboardStudentEmail(dashboardStudent.email);
+                } else {
+                    console.log("No student found with that email.");
+                }
             }
+            
+            // if (dashboardUser.data.listStudents.items.length > 0) {
+            //     setDashboardStudent(dashboardUser.data.listStudents.items[0]);
+            //     console.log(dashboardStudent); // Access the first student
+            // } else {
+            //     console.log("No student found with that email.");
+            // }
         } catch (error) {
             console.error('Error fetching user:', error);
         }
@@ -49,11 +82,12 @@ const Dashboard = (props) => {
     }, []);
 
     const onButtonClick2 = () => {
-        console.log('Current Student Email: ' + currentStudent.email);
-        console.log('Dashboard Student Email: ' + dashboardStudent.email);
+        console.log("working out functionality");
+        setBalanceError('still working on this feature ;)');
     }
 
     const findStudent = async (email) => {
+        console.log('Email: ' + email);
         const student = await client.graphql({
             query: listStudents,
             variables: {
@@ -107,14 +141,14 @@ const Dashboard = (props) => {
                     Personal
                 </div>
                 <div className="textContainer">
-                    {currentStudent.email}
+                    {currentStudentEmail}
                 </div>
                 <div className='balanceContainer'>
                     <div className='balanceText'>Total Balance</div>
-                    <div className='balanceAmount'>${currentStudent.balance}</div>
+                    <div className='balanceAmount'>${currentStudentBalance}</div>
                 </div>
             </div>
-            {currentStudent.email !== dashboardStudent.email ?
+            {currentStudentEmail !== dashboardStudentEmail ?
             <div className='studentContainer'>
                 <div className='dashboardTitle'> Viewing {dashboardStudent.name}</div>
                 <div className="textContainer">
