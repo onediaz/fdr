ed776a77a519b9b00fb6e81b159d4836f853f11f
onediaz <juand4535@gmail.com>
2024-05-23 21:12:42 -0500

    build fix

--- src/index.js
+++ src/index.js
@@ -8,21 +8,21 @@ import '@aws-amplify/ui-react/styles.css';
 import { ThemeProvider } from "@aws-amplify/ui-react";
 import { studioTheme } from "./ui-components";
 
-import amplifyconfig from './amplifyconfiguration.json';
-Amplify.configure(amplifyconfig);
+// import amplifyconfig from './amplifyconfiguration.json';
+// Amplify.configure(amplifyconfig);
 
-// const amplifyConfig = process.env.REACT_APP_FDR_AMPLIFY_CONFIG;
-// console.log('FDR_AMPLIFY_CONFIG:', amplifyConfig);
-// if (!amplifyConfig) {
-//   console.error('FDR_AMPLIFY_CONFIG is not set.');
-// } else {
-//   try {
-//     const parsedConfig = JSON.parse(amplifyConfig);
-//     Amplify.configure(parsedConfig);
-//   } catch (error) {
-//     console.error('Error parsing FDR_AMPLIFY_CONFIG:', error);
-//   }
-// }
+const amplifyConfig = process.env.REACT_APP_FDR_AMPLIFY_CONFIG;
+console.log('FDR_AMPLIFY_CONFIG:', amplifyConfig);
+if (!amplifyConfig) {
+  console.error('FDR_AMPLIFY_CONFIG is not set.');
+} else {
+  try {
+    const parsedConfig = JSON.parse(amplifyConfig);
+    Amplify.configure(parsedConfig);
+  } catch (error) {
+    console.error('Error parsing FDR_AMPLIFY_CONFIG:', error);
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
