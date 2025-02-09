44e5e4a89dfb54c5129d8f219fa0c9b22850118b
onediaz <juand4535@gmail.com>
2025-02-09 10:04:02 -0600

    Removing vite obfuscator

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ViteObfuscator } from 'vite-plugin-obfuscator';
 
 export default defineConfig({
   plugins: [
-    react(),
-    ViteObfuscator({
-        global: true, // Configure your settings here
-    })
+    react()
 ],
 });
